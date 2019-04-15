5abc0cd1fe84eef4a62e66cb11a0807b2b24deee
Grigoriy Stetsenko <stetsenko@softesis.com>
2019-04-15 13:53:14 +0300

    creating project

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# testng-example
+# trueautomation-java-example
 
-## How to run test: 
-* Run test ``mvn test``
+You must have installed TrueAutomation client, JDK 8 or later.
 
+## How to run test:
 
- 
+* Checkout project
+
+```
+clone https://github.com/stegrial/TA-801-w3c-without-TA.git
+```
+
+* Start driver
+
+In a separate command line window, go to the project directory and start a required driver by running the command:
+
+```bash
+  ./drivers/chromedriver --port=4444 // for ChromeDriver
+
+  ./drivers/geckodriver --port 4444 // for Geckodriver
+
+  safaridriver --port 4444 // for SafariDriver
+```
+
+* Run test
+
+```bash
+mvn -Dtest=exampleTest test
+```
--- src/test/java/exampleTest.java
+++ src/test/java/exampleTest.java
@@ -1,29 +1,29 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertTrue;
 
 public class exampleTest {
     private WebDriver driver;
-    private By loginBtn = By.cssSelector("a.login-btn");
+    private By loginBtn = By.xpath("//div[./span[text()='Login']]");
     private By signupBtn = By.cssSelector("div.sign-up-container > a");
     private By emailFl = By.name("email");
 
 
     @BeforeTest
-    public void beforeTest() {
-
-        /* Optional, if not specified, WebDriver will search your path for chromedriver.  *
-         *    System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");      */
-
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    public void beforeTest() throws MalformedURLException {
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), new DesiredCapabilities());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
@@ -32,7 +32,6 @@ public class exampleTest {
 
         driver.findElement(loginBtn).click();
         driver.findElement(signupBtn).click();
-
         driver.findElement(emailFl).sendKeys("your@mail.com");
     }
 
